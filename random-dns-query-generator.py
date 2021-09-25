2528b2ada08402963f09c4639044520cce62eda7
bitofsecurity <shakil.islam@gmail.com>
2021-09-25 16:47:48 +1000

    initial commit

---
+++ random-dns-query-generator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import dns.resolver
+import random, time
+dns.resolver.nameservers = ['localhost']
+
+letters= [chr(ord('a')+i) for i in range(26)]
+#types = ['NS', 'A', 'AAAA', 'MX']
+types = [ 'A' ] 
+
+qps = 1
+
+while True:
+        domain = ""
+        for i in range(2):
+                domain += random.choice(letters)
+        domain += ".com"
+        rt = random.choice(types)
+        print(domain+", "+rt)
+        try:
+#                dns.resolver.query(domain, rt)
+                dns.resolver.resolve(domain, rt)
+        except dns.resolver.NXDOMAIN:
+                pass
+        except dns.resolver.NoAnswer:
+                pass
+        except dns.resolver.Timeout:
+                pass
+        except dns.resolver.YXDOMAIN:
+                pass
+        except dns.resolver.NoNameservers:
+                pass
+        time.sleep(qps)
